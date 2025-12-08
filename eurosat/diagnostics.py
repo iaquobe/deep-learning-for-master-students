962dc92920e76777576b229070ab410f5dd751d9
Jakob Lambert-Hartmann <jakob.lamberthartmann.canada@gmail.com>
2025-12-08 19:18:25 +0100

    diagnostic plot

--- eurosat/diagnostics.py
+++ eurosat/diagnostics.py
@@ -42,17 +42,43 @@ def ranking_diagnostic(tensor_path, data_path, split_name):
                 plt.title("bot {}".format(k))
 
 
+def tpr_plot(metadata_path, tensor_path, data_path, split_name): 
+    true_class  = torch.tensor(np.array([int(l.split()[1]) for l in open(data_path / split_name)]))
+    logits      = torch.load(tensor_path)
+    pred_class  = logits.argmax(dim=3)
+    correct     = (pred_class == true_class)
+    tpr         = correct.sum(dim=2) / true_class.shape[0]
+
+    plt.plot(tpr[1,:])
+    plt.plot(tpr[0,:])
+
+    models = [l for l in open(metadata_path)]
+    for model_name, model_perf in zip(models, tpr): 
+        plt.plot(model_perf, label=model_name)
+        plt.ylabel("tpr")
+        plt.xlabel("epoch")
+    plt.legend()
+
+
 def main():
-    logits = torch.randn((4050, 10))
-    torch.save(logits, 'tensor.pt')
-    path = Path('tensor.pt')
+    # logits = torch.randn((4050, 10))
+    # torch.save(logits, 'test_tensor.pt')
+    # path = Path('test_tensor.pt')
+    # data_path = Path('./data/EuroSAT_RGB/')
+    # split_name = Path('test.txt')
+    # ranking_diagnostic(path, data_path, split_name)
+
+
+    logits = torch.randn((3, 10, 4050, 10))
+    torch.save(logits, 'val_tensor.pt')
+    path = Path('val_tensor.pt')
     data_path = Path('./data/EuroSAT_RGB/')
-    split_name = Path('test.txt')
-    ranking_diagnostic(path, data_path, split_name)
+    split = Path('val.txt')
+    models = Path('./models.txt')
+    tpr_plot(models, path, data_path, split)
 
 
 if __name__ == "__main__":
     main()
-
 
 
